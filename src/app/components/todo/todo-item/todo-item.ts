5738b675617de21c64646d106f7795bbccc11655
heba taha <amltaha866@yahoo.om>
2026-01-23 20:58:55 +0200

    todo UI

--- src/app/components/todo/todo-item/todo-item.ts
+++ src/app/components/todo/todo-item/todo-item.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon'
 
 @Component({
   selector: 'app-todo-item',
-  imports: [],
+  imports: [MatButtonModule,MatToolbarModule,MatIconModule],
   templateUrl: './todo-item.html',
   styleUrl: './todo-item.scss',
 })
--- src/app/components/todo/todo.ts
+++ src/app/components/todo/todo.ts
@@ -4,10 +4,13 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon'
 import { RouterLink } from '@angular/router';
-
+import { TodoSearch } from './todo-search/todo-search';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
 @Component({
   selector: 'app-todo',
-  imports: [MatToolbarModule,MatButtonModule,MatIconModule,TodoItem,RouterLink],
+  imports: [MatToolbarModule,MatButtonModule,MatIconModule,TodoItem,RouterLink,TodoSearch,FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './todo.html',
   styleUrl: './todo.scss',
 })
