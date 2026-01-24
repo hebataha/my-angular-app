129187bbb2b6ab212a593abe0763930473d9721e
heba taha <amltaha866@yahoo.om>
2026-01-25 00:49:12 +0200

    add example

--- src/app/todo/todo.ts
+++ src/app/todo/todo.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { TodoItem } from "./components/todo-item/todo-item";
+import { TodoItem } from "./todo-item/todo-item";
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon'
@@ -8,30 +8,26 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { TodoIem } from './interface/todo';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-todo',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, TodoItem, RouterLink, FormsModule, MatFormFieldModule, MatInputModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, TodoItem, RouterLink, FormsModule, MatFormFieldModule, MatInputModule,CommonModule],
   templateUrl: './todo.html',
   styleUrl: './todo.scss',
 })
 export class Todo implements OnInit{
-  todoArray: TodoIem[] = []
+  todoArray:any = [""]
   constructor() {
-    this.todoArray = [{
-      id: 1,
-      title: "todo 1",
-      description: "hi from todo 1",
-      isRead: false,
-
-    }]
+    this.todoArray = [""]
   }
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
 
   }
-  addTodo():void{
-  console.log("heba")
+  addTodo():any{
+  this.todoArray.push("heba")
   }
   valInput(): void{
 
