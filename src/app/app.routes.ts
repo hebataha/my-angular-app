7adcc9757be0cfffffe6e5e86663b50e6c4f48d1
heba taha <amltaha866@yahoo.om>
2026-01-31 12:35:28 +0200

    push forms component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Todo } from './todo/todo';
+import { Register } from './forms/components/register/register';
+import { Login } from './forms/components/login/login';
+import { ForgetPassword } from './forms/components/forget-password/forget-password';
 
 export const routes: Routes = [
   { path: "", component: Home },
   { path: "todoApp", component: Todo },
+  { path: "register", component: Register },
+  { path: "login", component: Login },
+  { path: "forgetPassword", component: ForgetPassword },
+
 ];
