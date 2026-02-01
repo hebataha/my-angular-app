d77e86df96c5039bfb1d1f9794bbfe393029edea
heba taha <amltaha866@yahoo.om>
2026-02-01 14:36:08 +0200

    login page

--- src/app/forms/components/login/login.ts
+++ src/app/forms/components/login/login.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { ContainerWrapper } from '../../../shared/container-wrapper/components/container-wrapper/container-wrapper';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ContainerWrapper,ReactiveFormsModule,JsonPipe],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
-
+  form =new FormGroup({
+    email: new FormControl("", [Validators.required, Validators.minLength(6) ,Validators.email]),
+    password: new FormControl("", [Validators.required, Validators.minLength(6)])
+  })
+  
+  get email() {
+    return this.form.controls.email
+  }
+    get password() {
+    return this.form.controls.password
+  }
 }
