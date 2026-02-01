91ed2400af8b32bbfcccb92463d2726a26958fa2
heba taha <amltaha866@yahoo.om>
2026-02-01 14:53:43 +0200

    forget password finished

--- src/app/forms/components/forget-password/forget-password.ts
+++ src/app/forms/components/forget-password/forget-password.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { ContainerWrapper } from '../../../shared/container-wrapper/components/container-wrapper/container-wrapper';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-forget-password',
-  imports: [ContainerWrapper],
+  imports: [ContainerWrapper,ReactiveFormsModule,JsonPipe],
   templateUrl: './forget-password.html',
   styleUrl: './forget-password.scss',
 })
 export class ForgetPassword {
-
+  form = new FormGroup({
+  email: new FormControl("", [Validators.required , Validators.minLength(6), Validators.email])
+})
+get email(){
+ return this.form.controls.email
+  }
+  
+  onSubmit() {
+    console.log(this.form.value);
+    
+  }
 }
