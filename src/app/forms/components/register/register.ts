6ac48436444318594b7330d147b9bf7de3309d17
heba taha <amltaha866@yahoo.om>
2026-02-01 13:51:59 +0200

    register page

--- src/app/forms/components/register/register.ts
+++ src/app/forms/components/register/register.ts
@@ -8,19 +8,27 @@ import { JsonPipe } from '@angular/common';
   imports: [ContainerWrapper, ReactiveFormsModule, JsonPipe],
   templateUrl: './register.html',
   styleUrl: './register.scss',
-}) 
+})
 export class Register {
-  touched: boolean = false
+  terms: boolean = false
   form = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.minLength(6), Validators.email]),
     password: new FormControl('', [Validators.required, Validators.minLength(8)]),
-    rePassword: new FormControl('', [Validators.required , Validators.minLength(8)]),
-
+    rePassword: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    city: new FormControl('', [Validators.required]),
+    imageUrl: new FormControl('', [Validators.required]),
+    terms: new FormControl(false, [Validators.required]),
+    gender: new FormControl('', [Validators.required]),
+    date: new FormControl('', [Validators.required])
   })
   constructor() {
-    this.touched = this.form.controls.email.touched;
 
   }
+
   onSubmit() {
+    console.log(this.form.value)
+  }
+  onTerms() {
+    this.terms = !this.terms
   }
 }
