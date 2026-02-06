aa0b568961edab501f9d98b6a781a12068294754
heba taha <amltaha866@yahoo.om>
2026-02-06 22:40:03 +0200

    object data service

--- src/app/shared/services/login-credentials.ts
+++ src/app/shared/services/login-credentials.ts
@@ -4,7 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class LoginCredentials {
-    loginDataService: object = {
-
+  loginDataService: object = {
+    email: "",
+    password: "",
   }
 }
