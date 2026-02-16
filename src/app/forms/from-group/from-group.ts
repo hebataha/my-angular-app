0caa7efb3e1406bb3c2ab34f1a89b59dc6e511cf
heba taha <amltaha866@yahoo.om>
2026-02-16 16:35:56 +0200

    form fields and control them

--- src/app/forms/from-group/from-group.ts
+++ src/app/forms/from-group/from-group.ts
@@ -1,11 +1,31 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-from-group',
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './from-group.html',
   styleUrl: './from-group.scss',
 })
 export class FromGroup {
 
+  form = new FormGroup({
+    name: new FormControl("", [Validators.required]),
+    address: new FormControl("", [Validators.required]),
+    product: new FormGroup({
+      proName: new FormControl("", [Validators.required]),
+      proQuantity: new FormControl(0, [Validators.required]),
+      proPrice: new FormControl(0, [Validators.required]),
+
+    }),
+    notes: new FormControl("", [Validators.required]),
+
+
+  })
+
+  submit() {
+    console.log(this.form.value);
+
+  }
 }
