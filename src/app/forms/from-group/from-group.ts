f35c907c34e4d1aea9809820b008644fa52ab0e2
heba taha <amltaha866@yahoo.om>
2026-03-04 13:16:53 +0200

    solve price issue - subscribe

--- src/app/forms/from-group/from-group.ts
+++ src/app/forms/from-group/from-group.ts
@@ -1,6 +1,13 @@
 import { CommonModule, JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormArray, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Product } from './interface/product';
 
 @Component({
@@ -9,73 +16,97 @@ import { Product } from './interface/product';
   templateUrl: './from-group.html',
   styleUrl: './from-group.scss',
 })
-export class FromGroup {
+export class FromGroup implements OnInit {
   productInfo: Product[] = [];
-  constructor() {
 
-    this.productInfo = [{
-      name: "laptop",
-      price: 4000,
+  ngOnInit() {
 
-    },
-    {
-      name: "Ipad",
-      price: 5000
-    },
-    {
-      name: "Iphone",
-      price: 15000
-    }
-    ]
-
-
-  }
-
-  selectChange() {
-    this.allProducts.at(0).valueChanges.subscribe(newValue => {
-      console.log(newValue.proName);
-    });
-  }
-  form = new FormGroup({
-    name: new FormControl("", [Validators.required, Validators.minLength(4)]),
-    address: new FormControl("", [Validators.required]),
-    notes: new FormControl("", [Validators.required]),
-    allProducts: new FormArray([
-      new FormGroup({
-        proName: new FormControl<any>(null, [Validators.required]),
-        proQuantity: new FormControl(1, [Validators.required, Validators.min(1)]),
-        proPrice: new FormControl(0, [Validators.required]),
-      })
-    ])
-
-  })
-  get allProducts(): FormArray {
-    return this.form.get('allProducts') as FormArray;
-  }
-  createProductGroup(): FormGroup {
-    return new FormGroup({
-      proName: new FormControl<any>(null, [Validators.required]),
-      proQuantity: new FormControl(1, [Validators.required, Validators.min(1)]),
-      proPrice: new FormControl(0, [Validators.required]),
+    const firstGroup = this.allProducts.at(0);
+    firstGroup.get('proName')?.valueChanges.subscribe(value => {
+      value ? firstGroup.get('proPrice')?.setValue(value.price) : ""
     })
   }
 
+  constructor() {
+    this.productInfo = [
+      {
+        name: 'laptop',
+        price: 4000,
+      },
+      {
+        name: 'Ipad',
+        price: 5000,
+      },
+      {
+        name: 'Iphone',
+        price: 15000,
+      },
+    ];
+  }
+
+  selectChange() {
+    // this.allProducts.at(0).valueChanges.subscribe(newValue => {
+    //   console.log(newValue.proName);
+    // });
+  }
+
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    address: new FormControl('', [Validators.required]),
+    notes: new FormControl('', [Validators.required]),
+    allProducts: new FormArray([
+      new FormGroup({
+        proName: new FormControl<any>(null, [Validators.required]),
+        proQuantity: new FormControl(1, [
+          Validators.required,
+          Validators.min(1),
+        ]),
+        proPrice: new FormControl(0, [Validators.required]),
+      }),
+    ]),
+  });
+  get allProducts(): FormArray {
+    return this.form.get('allProducts') as FormArray;
+  }
+  // createProductGroup(): FormGroup {
+
+  //   const group = new FormGroup({
+  //     proName: new FormControl<Product | null>(null, Validators.required),
+  //     proQuantity: new FormControl(1, [Validators.required, Validators.min(1)]),
+  //     proPrice: new FormControl(0, Validators.required),
+  //   });
+
+  //   // نسمع لتغيير اسم المنتج
+  //   group.get('proName')?.valueChanges.subscribe(product => {
+  //     if (product) {
+  //       group.get('proPrice')?.setValue(product.price);
+  //     }
+  //   });
+
+  //   return group;
+  // }
+
   submit() {
     console.log(this.form.value);
-
   }
 
   add() {
-    (this.form.get('allProducts') as FormArray).push(this.createProductGroup())
-    // this.allProducts.at(0).get('proName')?.value
-    console.log(this.allProducts.value);
+    const group = new FormGroup({
+      proName: new FormControl<Product | null>(null, Validators.required),
+      proQuantity: new FormControl(1, [Validators.required, Validators.min(1)]),
+      proPrice: new FormControl(0, Validators.required),
+    });
 
+    this.allProducts.push(group);
+
+    group.get('proName')?.valueChanges.subscribe((value) => {
+      value ? group.get('proPrice')?.setValue(value?.price) : '';
+    });
   }
 
   delete(i: number) {
     this.allProducts.removeAt(i);
 
-    console.log(i ,this.allProducts);
+    console.log(i, this.allProducts);
   }
-
 }
