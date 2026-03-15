795af5a71b24b904b675139c6767e5489335cf6d
heba taha <amltaha866@yahoo.om>
2026-03-15 20:49:25 +0200

    handel 4 data types

--- src/app/product-type-stepper/product-type-stepper/product-type-stepper.ts
+++ src/app/product-type-stepper/product-type-stepper/product-type-stepper.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { servicePopup } from '../../shared/popup/service-popup';
-
+import { ProductType } from '../../shared/enums/product-type';
+import { JsonPipe } from '@angular/common';
 @Component({
   selector: 'app-product-type-stepper',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './product-type-stepper.html',
   styleUrl: './product-type-stepper.scss',
 })
 export class ProductTypeStepper {
   stepNum: number = 1;
+  ProductType = ProductType;
+  ngOnInit() {  }
   constructor(private _servicePopup: servicePopup) {
     this._servicePopup.popupNumber.subscribe((val) => this.stepNum = val)
   }
---
+++ src/app/shared/enums/product-type.ts
@@ -0,0 +1,6 @@
+export enum ProductType {
+  VOICE = 1,
+  DATA = 2,
+  LANDLINE = 3,
+  ADSL = 4
+}
