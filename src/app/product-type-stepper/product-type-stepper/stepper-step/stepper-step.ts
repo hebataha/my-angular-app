d43a99654f670b9b61ea7811ffa42ce15064f8e1
heba taha <amltaha866@yahoo.om>
2026-04-02 21:43:10 +0200

    handle steps color active and  complete

--- src/app/product-type-stepper/product-type-stepper/stepper-step/stepper-step.ts
+++ src/app/product-type-stepper/product-type-stepper/stepper-step/stepper-step.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ProductType } from '../../../shared/enums/product-type';
-import { servicePopup } from '../../../shared/popup/service-popup';
+import { CountStepperService } from '../services/count-stepper-service';
 
 @Component({
   selector: 'app-stepper-step',
@@ -10,8 +10,15 @@ import { servicePopup } from '../../../shared/popup/service-popup';
 })
 export class StepperStep {
   stepNum: number = 1;
+  step1: number = 1;
+  step2: number = 2;
+  step3: number = 3;
+  step4: number = 4;
+  step5: number = 5;
+
+
   ProductType = ProductType;
-    constructor(private _servicePopup: servicePopup) {
-      this._servicePopup.popupNumber.subscribe((val) => this.stepNum = val)
-    }
+  constructor(private _countStepperService:CountStepperService) {
+    this._countStepperService.stepperCount.subscribe((val) => this.stepNum = val)
+  }
 }
