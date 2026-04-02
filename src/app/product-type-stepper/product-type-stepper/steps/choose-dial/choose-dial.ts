4b2d755b30c90387385178f1fc31a71b074419c7
heba taha <amltaha866@yahoo.om>
2026-04-02 20:58:58 +0200

    edit choose dial UI

--- src/app/product-type-stepper/product-type-stepper/steps/choose-dial/choose-dial.ts
+++ src/app/product-type-stepper/product-type-stepper/steps/choose-dial/choose-dial.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-choose-dial',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './choose-dial.html',
   styleUrl: './choose-dial.scss',
 })
 export class ChooseDial {
-
+  selectedRadio: string = "pool";
 }
