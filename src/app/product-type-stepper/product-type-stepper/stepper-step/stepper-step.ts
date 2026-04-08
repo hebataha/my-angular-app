6acd6f321129da9f87b107019d7ce83cd8fc938e
heba taha <amltaha866@yahoo.om>
2026-04-08 17:21:47 +0200

    edit stepper step shown

--- src/app/product-type-stepper/product-type-stepper/stepper-step/stepper-step.ts
+++ src/app/product-type-stepper/product-type-stepper/stepper-step/stepper-step.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ProductType } from '../../../shared/enums/product-type';
 import { CountStepperService } from '../services/count-stepper-service';
+import { servicePopup } from '../../../shared/popup/service-popup';
 
 @Component({
   selector: 'app-stepper-step',
@@ -10,6 +11,8 @@ import { CountStepperService } from '../services/count-stepper-service';
 })
 export class StepperStep {
   stepNum: number = 1;
+    typeNum: number = 1;
+
   step1: number = 1;
   step2: number = 2;
   step3: number = 3;
@@ -18,7 +21,9 @@ export class StepperStep {
 
 
   ProductType = ProductType;
-  constructor(private _countStepperService:CountStepperService) {
-    this._countStepperService.stepperCount.subscribe((val) => this.stepNum = val)
+  constructor(private _countStepperService: CountStepperService, private _servicePopup: servicePopup) {
+    this._countStepperService.stepperCount.subscribe((val) => this.stepNum = val);
+    this._servicePopup.popupNumber.subscribe((val) => this.typeNum = val);
+
   }
 }
