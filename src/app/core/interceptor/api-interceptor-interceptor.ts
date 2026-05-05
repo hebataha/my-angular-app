50fdb3011eb9a083fe2a09f08b4b90c89019eff8
heba taha <amltaha866@yahoo.om>
2026-05-05 18:36:17 +0300

    apiInterceptorInterceptor

--- src/app/core/interceptor/api-interceptor-interceptor.ts
+++ src/app/core/interceptor/api-interceptor-interceptor.ts
@@ -1,5 +1,20 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const apiInterceptorInterceptor: HttpInterceptorFn = (req, next) => {
+  const token =
+    typeof window !== 'undefined'
+      ? localStorage.getItem('token')
+      : null;
+
+  if (token) {
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    console.log('Authorization header:', req.headers.get('Authorization'));
+  }
+
   return next(req);
 };
